7b69eb307f5a62798f59b8b6bf342c8edfba0769
FVIFVIFVI <139038166+FVIFVIFVI@users.noreply.github.com>
2024-06-21 15:14:23 +0300

    1

--- TODOAPI/Controllers/words.cs
+++ TODOAPI/Controllers/words.cs
@@ -3,6 +3,7 @@ using WordsApi.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace WordsApi.Controllers
 {
@@ -17,53 +18,84 @@ namespace WordsApi.Controllers
         public WordsController(WordsService wordsService) =>
             _wordsService = wordsService;
 
-        [HttpGet]
-        public async Task<List<Words>> Get() {
-         Console.WriteLine("ddddddddddddd");
-           return await _wordsService.GetAsync();}
-            
-
         [HttpGet("{name}")]
         public async Task<List<Tuple<string, int>>> Get(string name)
-
         {
-
-
-            var words = await _wordsService.GetAsync(name);
-
-                     Console.WriteLine(name,"ggggggggg");
-
-
-
-
-
-            List<Tuple<string, int>> stringList = new List<Tuple<string, int>>();
-
-            List<int> countList = new List<int>();
+            //Console.WriteLine($"{name}");
+            var urlsData = new Dictionary<string, int>();
+            var url1 = new Dictionary<string, int>();
             
+            List<Tuple<string, int>> stringList = new List<Tuple<string, int>>();
+           // Console.WriteLine(8883);
+            var listwords = name.Split(new[] { '\r', '\n', ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
-            if (words is null)
+            //Console.WriteLine(8885);
+            
+            foreach (var i1 in listwords)
             {
-                return stringList;
+                //Console.WriteLine(88857);
+                //Console.WriteLine(i1);
+                var excludedUrls = new HashSet<string>();
+                var words = await _wordsService.GetAsync(i1);
+                if (words == null || words.Dict == null)
+                {
+                    //Console.WriteLine($"No data found for {i1}");
+                    continue;
+                }
+                //Console.WriteLine("56565656");
+
+                foreach (var result in words.Dict)
+                {
+                    Console.WriteLine(result.Url + "9");
+                    if (excludedUrls.Contains(result.Url))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        excludedUrls.Add(result.Url);
+                    }
+
+                    //Console.WriteLine(12);
+                    if (!url1.ContainsKey(result.Url))
+                    {
+                        url1[result.Url] = 0;
+                        urlsData[result.Url] = 0;
+                    }
+                    url1[result.Url]++;
+                    urlsData[result.Url] += result.Count;
+                }
+            }
+            Console.WriteLine("343434444");
+
+            foreach (var url in url1.Keys.ToList())
+            {
+                Console.WriteLine(232);
+                Console.WriteLine(url);
+                Console.WriteLine(url1[url]);
+                Console.WriteLine(listwords.Length);
+                if (url1[url] < listwords.Length)
+                {
+                    url1.Remove(url);
+                    //urlsData.Remove(url);
+                }
             }
 
-            int sizeoflist = words.Dict?.Count ?? 0;
-            foreach (var i in words.Dict)
-        
-                { 
-                     stringList.Add(new Tuple<string, int>(i.Url, i.Count));
-                    
-                     
+            Console.WriteLine(name, "ggggggggg");
 
-                }
+            int name1 = listwords.Length;
 
-
-
+            foreach (var word1 in url1.Keys)
+            {
+                stringList.Add(new Tuple<string, int>(word1, urlsData[word1] / name1));
+            }
 
             var sortedList = stringList.OrderByDescending(tuple => tuple.Item2).ToList();
 
             return sortedList;
         }
+
+
 
 
         [HttpPost]
